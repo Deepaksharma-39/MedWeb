4ecad316bcb11be6087c1e023a031b062c53ed4a
Heena-Kapoor <hkkapoor257@gmail.com>
2023-12-07 12:16:56 +0530

    Home Page

--- src/pages/home/HomeMedicalEquipment.js
+++ src/pages/home/HomeMedicalEquipment.js
@@ -4,9 +4,12 @@ import LayoutOne from "../../layouts/LayoutOne";
 import HeroSliderTwentyFive from "../../wrappers/hero-slider/HeroSliderTwentyFive";
 import BannerTwentyFive from "../../wrappers/banner/BannerTwentyFive";
 import FeatureIconFive from "../../wrappers/feature-icon/FeatureIconFive";
-import BannerTwentySix from "../../wrappers/banner/BannerTwentySix";
-// import TabProductSixteen from "../../wrappers/product/TabProductSixteen";
-//import TabProductSixteen from "../../wrappers/product/TabProductSixteen";
+ import BannerTwentySix from "../../wrappers/banner/BannerTwentySix";
+import ProductSlider from "../../wrappers/product/ProductSlider";
+import BlogFeaturedFour from "../../wrappers/blog-featured/BlogFeaturedFour";
+import MedicalContact from "../../components/contact/MedicalContact";
+import TabProductSixteen from "../../wrappers/product/TabProductSixteen";
+
 
 
 const HomeMedicalEquipment = () => {
@@ -28,9 +31,16 @@ const HomeMedicalEquipment = () => {
         {/* feature icon */}
         <FeatureIconFive spaceBottomClass="pb-100" />
          {/* tab product */}
-         {/* <TabProductSixteen spaceBottomClass="pb-70" category="medical" /> */}
+         <TabProductSixteen spaceBottomClass="pb-70" category="medical" />
          {/* banner */}
          <BannerTwentySix spaceBottomClass="pb-70" />
+         {/* product slider */}
+        <ProductSlider category="medical" />
+        {/* blog */}
+        <BlogFeaturedFour spaceBottomClass="pb-55" spaceTopClass="pt-95" />
+        {/* contact */}
+        <MedicalContact />
+
       </LayoutOne>
     </Fragment>
   );
